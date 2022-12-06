de68eb6a5e8487ffb48ca1073bad2095464888b6
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2022-12-06 15:01:10 -0600

    replit doesn't do the next part
    Need to be able to test and try .env and replit doesn't do that

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
 let express = require('express');
 let app = express();
+let express = require('express');
+let app = express();
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", function(req, res) {
+  res.json({message : "Hello json"});
+});
 
 
 
